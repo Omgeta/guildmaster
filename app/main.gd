cf7e2550369275ecabe58ab3259dba2950f94439
Omgeta <anooptiger@hotmail.com>
2025-05-31 01:59:06 +0800

    feat: cutscenes

--- app/main.gd
+++ app/main.gd
@@ -4,15 +4,15 @@ extends Node
 @onready var gui = $GUI
 
 
-func fade_to_screen(path: String):
+func fade_to_scene(scene: Node):
 	var tween = create_tween()
 	tween.tween_property(fade_layer, "modulate:a", 1.0, 0.5)
 	await tween.finished
 
 	if gui.get_child_count() > 0:
 		gui.get_child(0).queue_free()
-	var new_scene = load(path).instantiate()
-	gui.add_child(new_scene)
+
+	gui.add_child(scene)
 
 	tween = create_tween()
 	tween.tween_property(fade_layer, "modulate:a", 0.0, 0.5)
@@ -22,8 +22,28 @@ func _on_splash_screen_scene_finished() -> void:
 	var tween = create_tween()
 	tween.tween_property(gui, "modulate:a", 1.0, 1.0)
 	await tween.finished
+
 	var new_game := not GameState.load_game()
+
 	if new_game:
-		fade_to_screen("res://core/cutscenes/intro.tscn")
+		var cutscene = load("res://core/cutscenes/cutscene_template.tscn").instantiate().init(
+			(
+				[
+					{"text": "You wake up in a desolate land, with only a tower infront of you."},
+					{"text": "Your instincts scream at you."},
+					{"text": "You must climb, or you won't be able to return to your world."},
+					{"text": "Suddenly, a light appears from behind you."},
+					{"text": "You turn to see the light take shape into a human..."}
+				]
+				as Array[Dictionary]
+			)
+		)
+		cutscene.connect("cutscene_finished", fade_to_main_game)
+		await fade_to_scene(cutscene)
 	else:
-		fade_to_screen("res://core/screens/gacha_screen.tscn")
+		await fade_to_main_game()
+
+
+func fade_to_main_game():
+	var next_scene = load("res://core/screens/gacha/gacha_screen.tscn").instantiate()
+	await fade_to_scene(next_scene)
